56e35bedee26f341f73ec703fa43ec086b6c30fc
CAROLINA_HASHIMOTO <carolina.hashimoto@fatec.sp.gov.br>
2023-08-31 18:02:47 -0300

    ap06(java): teste do livro de notas

---
+++ TesteLivroDeNotas.java
@@ -0,0 +1,13 @@
+public class TesteLivroDeNotas{
+    public static void main(String args []){
+        //variável de referência
+        //malloc
+        LivroDeNotas livroDeNotas = new LivroDeNotas();
+        livroDeNotas.exibirMensagem();
+        System.out.println("Até logo");
+    }
+}
+
+// nome da classe "LivroDeNotas" = tipo da variável "livroDeNotas" (linha 45)
+//new operação de construção de objeto +- o que o malloc faz
+//ponteiro aponta ao objeto LivroDeNotas
